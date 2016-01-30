4d6840e1d6ab6dadaa554ed70504b74ef7a749a0
inuscript <inuscript@gmail.com>
2016-01-31 00:59:24 +0900

    add

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const logoApi = require("./api/logo")
 const denodeify = require("denodeify")
 const pngparse = require("pngparse")
+const parse = require("pixelbank")
+
+const parsePng = function(png){
+  return denodeify(pngparse.parse)(png)
+    .then( png => {
+      console.log(png.data.length)
+      // console.log(parse(png))
+    })
+}
 module.exports = function(q){
   logoApi(q).then( res => {
     return res.data
   }).then( data => {
-    return denodeify(pngparse.parse)(data)
-  }).then( png => {
-    console.log(png.getPixel(10,10))
+    return parsePng(data)
   }).catch( e => {
     console.trace(e)
     throw e
